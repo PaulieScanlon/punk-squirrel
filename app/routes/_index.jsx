b330fc8ed3e999f933ee7a36690ba2fdae4e77ce
Paul Scanlon <pauliescanlon@gmail.com>
2023-11-22 11:47:56 -0500

    refactor: remove edge functions - use serverless functions

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,19 +1,19 @@
-// import { useOutletContext } from '@remix-run/react';
+import { useOutletContext } from '@remix-run/react';
 
 import DefaultHeader from '../layouts/default-header';
 import DefaultLayout from '../layouts/default-layout';
 
-// import SignInWithGitHub from '../components/sign-with-github-button';
+import SignInWithGitHub from '../components/sign-with-github-button';
 
 const Page = () => {
-  // const { env, supabase, session } = useOutletContext();
+  const { authRedirect, supabase, session } = useOutletContext();
 
   return (
     <>
       <DefaultHeader />
       <DefaultLayout>
         <h1>Index</h1>
-        {/* <SignInWithGitHub /> */}
+        <SignInWithGitHub authRedirect={authRedirect} supabase={supabase} session={session} />
       </DefaultLayout>
     </>
   );
