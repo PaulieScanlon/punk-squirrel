19e90599f447d70e8036b5ade4fa62912d1c3f2b
Paul Scanlon <pauliescanlon@gmail.com>
2023-11-21 20:43:55 -0500

    style: add 600 font weight

--- app/root.jsx
+++ app/root.jsx
@@ -6,6 +6,7 @@ import plusJakartaSans200 from '@fontsource/plus-jakarta-sans/200.css';
 import plusJakartaSans300 from '@fontsource/plus-jakarta-sans/300.css';
 import plusJakartaSans400 from '@fontsource/plus-jakarta-sans/400.css';
 import plusJakartaSans500 from '@fontsource/plus-jakarta-sans/500.css';
+import plusJakartaSans600 from '@fontsource/plus-jakarta-sans/600.css';
 import plusJakartaSans700 from '@fontsource/plus-jakarta-sans/700.css';
 import plusJakartaSans800 from '@fontsource/plus-jakarta-sans/800.css';
 
@@ -23,6 +24,7 @@ export const links = () => [
   { rel: 'stylesheet', href: plusJakartaSans300 },
   { rel: 'stylesheet', href: plusJakartaSans400 },
   { rel: 'stylesheet', href: plusJakartaSans500 },
+  { rel: 'stylesheet', href: plusJakartaSans600 },
   { rel: 'stylesheet', href: plusJakartaSans700 },
   { rel: 'stylesheet', href: plusJakartaSans800 },
 ];
